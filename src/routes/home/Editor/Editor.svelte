62ddbf0439412619b3001308bfaefaacce0d6582
finnhvman <szabo.bence2@gmail.com>
2023-02-19 17:28:24 +0100

    Integrate Ace editor

--- src/routes/home/Editor/Editor.svelte
+++ src/routes/home/Editor/Editor.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import ace from 'ace-builds';
+	import 'ace-builds/src-noconflict/ext-language_tools';
+	import 'ace-builds/src-noconflict/mode-svg';
+	import 'ace-builds/src-noconflict/theme-cobalt';
+	import editor from './editor';
 	import code from '../code';
+
+	let container: HTMLDivElement;
+
+	onMount(() => {
+		if (container) {
+			$editor = ace.edit(container, {
+				customScrollbar: true,
+				enableBasicAutocompletion: true,
+				fontSize: 16,
+				mode: 'ace/mode/svg',
+				useSoftTabs: true,
+				tabSize: 2,
+				theme: 'ace/theme/cobalt',
+				useWorker: false
+			});
+			$editor.on('change', () => ($code = $editor.getValue()));
+		}
+	});
+
+	onDestroy(() => {
+		$editor?.destroy();
+	});
 </script>
 
-<textarea bind:value={$code} />
+<div bind:this={container} />
 
 <style>
-	textarea {
-		width: 100%;
-		height: 100%;
-	}
+    div {
+        width: 100%;
+        height: 100%;
+    }
 </style>
