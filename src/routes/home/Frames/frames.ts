af108d8094238fd1e16af3dc2f2277ff9bba2077
finnhvman <szabo.bence2@gmail.com>
2023-02-20 20:25:35 +0100

    Use localStorage

--- src/routes/home/Frames/frames.ts
+++ src/routes/home/Frames/frames.ts
@@ -1,9 +1,17 @@
 import { writable } from 'svelte/store';
+import { browser } from "$app/environment";
 import type Frame from './Frame';
 
-export default writable<Frame[]>([
-	{
-		title: '',
-		code: ''
-	}
-]);
+function getFrames() {
+	const frames = (browser && localStorage?.getItem('frames')) || '[{"title":"","code":""}]';
+	return JSON.parse(frames);
+}
+
+function setFrames(frames: Frame[]) {
+	browser && localStorage?.setItem('frames', JSON.stringify(frames));
+}
+
+const store = writable<Frame[]>(getFrames());
+store.subscribe(setFrames);
+
+export default store;
